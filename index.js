d1831bbdc34ffcf706c614c3f78bd0de717d424f
AndradeTH <deandradeoliveira2@gmail.com>
2024-10-10 19:14:02 -0300

    Segunda funcionalidade

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const express = require('express')
 const mysql = require('mysql2')
 const { engine } = require('express-handlebars')
 const index = express();
+const fileupload = require('express-fileupload')
+const fs = require('fs')
+
+// Habilitando upload de arquivos
+index.use(fileupload())
 
 // Adicionando bootstrap
 index.use('/bootstrap', express.static('./node_modules/bootstrap/dist'))
+
+// Referenciar a pasta imagens
+index.use('/imagens', express.static('./imagens'))
 
 //Configuração do handlebars
 index.engine('handlebars', engine());
@@ -33,25 +41,54 @@ conexao.connect(function(erro){
 
 // Rota Principal
 index.get('/', function(req, res){
-    res.render('formulario')
+    
+    let sql = 'SELECT * FROM produtos'
+
+    conexao.query(sql, function(erro, retorno){
+        res.render('formulario', {produtos:retorno})
+    })
 })
+
 
 // Rota de Cadastro
 index.post('/cadastrar', function(req,res){
 
     let nome = req.body.nome
     let valor = req.body.valor
+    let imagem = req.files.imagem.name
 
-    let sql = `INSERT INTO produtos (nome, valor) VALUES ('${nome}', ${valor})`
+    let sql = `INSERT INTO produtos (nome, valor, imagem) VALUES ('${nome}', ${valor}, '${imagem}')`
 
-    conexao.query(sql, function(erro, funcionando){
+
+
+    conexao.query(sql, function(erro, retorno){
         if(erro) throw erro
-        console.log(funcionando)
+        req.files.imagem.mv(__dirname+'/imagens/'+req.files.imagem.name)
+        console.log(retorno)
     })
 
     // Retorna para rota principal
     res.redirect('/')
 })
+
+// Rota de remoção
+index.get('/remover/:codigo&:imagem', function(req, res){
+    
+    let sql = `DELETE FROM produtos WHERE codigo = ${req.params.codigo}`
+
+    conexao.query(sql, function(erro, retorno){
+        if(erro) throw erro
+
+        fs.unlink(__dirname+'/imagens/'+req.params.imagem, (erro_inesperado)=>{
+            console.log('Produto removido')
+        })
+    })
+
+    // Redirecinando para rota proncipal
+    res.redirect('/')
+})
+    
+
 
 // Servidor
 index.listen(8080)
